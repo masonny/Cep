c18f497c1d3e31e7e1acd9904a1dc6fba34605f4
mason <1689699455@qq.com>
2023-04-14 18:01:26 +0800

    Init

---
+++ src/test/java/com/xhu/cep/CepApplicationTests.java
@@ -0,0 +1,12 @@
+package com.xhu.cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
